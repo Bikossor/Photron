a338a0252d0ff4a85b88e1ea70bbd9765e4c9ca3
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2017-07-26 18:06:32 +0200

    Added a README

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # ALightBox
-Coming soon...
+ALightBox is an easy-to-use jQuery-Plugin which provides a lightbox for your website.
+
+## Features
+- Supports 9 different languages
+	- German
+	- English
+	- French
+	- Spanish
+	- Italian
+	- Chinese
+	- Russian
+	- Danish
+	- Dutch
+- Supports images
+- Supports videos from YouTube
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Deployment
+- *Coming soon...*
+
+## Built With
+- **Nothing else**
+
+## Versioning
+I use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/Bikossor/ALightBox/tags).
+
+## Authors
+- **André Lichtenthäler** - *Initial work* - [Bikossor](https://github.com/Bikossor)
+
+## License
+This project is licensed under the MIT License. See the [LICENSE.md](LICENSE.md) file for more details.
+
+## Changelog
+### Version 0.1.0: (2nd July, 2017)
+- First introduction
+
+## Acknowledgments
+- *Coming soon...*
--- src/ALightBox.js
+++ src/ALightBox.js
@@ -1,30 +1,26 @@
 (function($) {
 	$.fn.ALightBox = function(options) {
 
+		/*-- Plugin defaults --*/
 		var defaults = {
 			effectTime: 200,
 			showImageTitle: true,
 			showGalleryTitle: true,
 			language: navigator.language.substr(0,2),
-			roundRobin: false
+			roundRobin: true,
+			showYoutubeThumbnails: false
 		}
 
-		/*--Variables--*/
+		/*-- Variables --*/
 		var settings = $.extend({}, defaults, options);
-		var imgActive = {};
-		var imgSource = "";
-		var imgSelector = "#alb_overlay #alb_content img";
 		var docTitle = document.title;
 		var galleryTitle = "";
-
-		/*--New variables (02.07.2017)--*/
-		var itemSelector = "img";
-		var itemCurrent = 0;
+		var itemSelector = ".alb_item";
+		var index = 0;
 		var items = $(this).find(itemSelector);
-		var lightbox_content = "<img />";
+		var totalItems = items.length;
 
-		//https://www.youtube.com/embed/  ?badge=0&autoplay=1&html5=1
-
+		/*-- Languages --*/
 		var language = {
 			de: {prev: "Vorheriges Bild", next: "Nächstes Bild", close: "Schließen"},
 			en: {prev: "Previous image", next: "Next image", close: "Close"},
@@ -37,43 +33,46 @@
 			nl: {prev: "Vorig beeld", next: "Volgend beeld", close: "Dichtbij"}
 		}
 
-		$('body').append(`
-			<div id="alb_overlay">
-				<nav>
-					<span id="alb_icon_prev" title="${language[settings.language]["prev"]}"></span>
-					<span id="alb_icon_close" title="${language[settings.language]["close"]}"></span>
-					<span id="alb_icon_next" title="${language[settings.language]["next"]}"></span>
-				</nav>
-				<div id="alb_content">
-					${lightbox_content}
-				</div>
-				<div id="alb_footer"></div>
-			</div>
-		`);
+		for(var i = 0; i < totalItems; i++) {
+			/*-- Hide all hidden elements --*/
+			if($(items[i]).data('hidden') === true) {
+				$(items[i]).css({
+					display: 'none'
+				});
+			}
 
-		function Open(lbObject) {
-			imgActive = lbObject;
-			imgSource = imgActive.attr('src');
-			galleryTitle = imgActive.parent().attr('data-title');
+			var tag = $(items[i])[0].localName;
 
-			Update(imgActive);
-			$(imgSelector).attr('src', imgSource);
+			/*-- Is the tag an a-tag? --*/
+			if(tag == 'a' && settings.showYoutubeThumbnails) {
+				var videoID = $(items[i]).attr('href').match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/)[2];
+
+				$(items[i]).html(`<img src="https://i.ytimg.com/vi/${videoID}/maxresdefault.jpg"/>`)
+				$('#alb_content').html(`<iframe src="https://www.youtube.com/embed/${videoID}?badge=0&html5=1" width="1280" height="720" frameborder="0" allowfullscreen></iframe>`);
+			}
+		}
+
+		/*-- Append the actual lightbox to the HTML-body --*/
+		$('body').append(`<div id="alb_overlay"><nav><span id="alb_icon_prev" title="${language[settings.language]["prev"]}"></span><span id="alb_icon_close" title="${language[settings.language]["close"]}"></span><span id="alb_icon_next" title="${language[settings.language]["next"]}"></span></nav><div id="alb_content"></div><div id="alb_footer"></div></div>`);
+
+		function Open(obj) {
+			galleryTitle = obj.parent().data('title');
+			index = $(obj).parent().children(itemSelector).index(obj);
+
+			Update();
 			$('#alb_overlay').fadeIn(settings.effectTime);
 		}
 
-		function Update(lbObject) {
-			if(lbObject.attr('data-title') && settings.showImageTitle) {
-				document.title = docTitle + " - " + lbObject.attr('data-title');
-			}
+		function Update() {
+			LoadContent($(items[index]));
 
-			itemCurrent = $(lbObject).parent().children(itemSelector).index(lbObject) + 1;
+			if($(items[index]).data('title') && settings.showImageTitle)
+				document.title = docTitle + " - " + $(items[index]).data('title');
 
-			if(lbObject.parent().attr('data-title') && settings.showGalleryTitle) {
-				alb_footer.innerHTML = galleryTitle + ": " + itemCurrent + " / " + items.length;
-			}
-			else {
-				alb_footer.innerHTML = itemCurrent + " / " + items.length;
-			}
+			if($(items[index]).parent().data('title') && settings.showGalleryTitle)
+				$('#alb_footer').html(galleryTitle + ": " + (index + 1) + " / " + totalItems);
+			else
+				$('#alb_footer').html(index + " / " + totalItems);
 		}
 
 		function Close() {
@@ -83,75 +82,53 @@
 				document.title = docTitle;
 		}
 
-		function NextItem(lbObject) {
-			if($(lbObject).next().is(itemSelector)) {
-				imgActive = $(lbObject).next();
-				imgSource = imgActive.attr('src');
-				Update(imgActive);
-				$(imgSelector).attr('src', imgSource);
+		function Next() {
+			if(index < totalItems - 1) {
+				index++;
+				Update();
 			}
 			else if(settings.roundRobin) {
-				imgActive = $(lbObject).parent().children(itemSelector).first();
-				imgSource = imgActive.attr('src');
-				Update(imgActive);
-				$(imgSelector).attr('src', imgSource);
+				index = 0;
+				Update();
 			}
 		}
 
-		function PreviousItem(lbObject) {
-			if($(lbObject).prev().is(itemSelector)) {
-				imgActive = $(lbObject).prev();
-				imgSource = imgActive.attr('src');
-				Update(imgActive);
-				$(imgSelector).attr('src', imgSource);
+		function Previous() {
+			if(index > 0) {
+				index--;
+				Update();
 			}
 			else if(settings.roundRobin) {
-				imgActive = $(lbObject).parent().children(itemSelector).last();
-				imgSource = imgActive.attr('src');
-				Update(imgActive);
-				$(imgSelector).attr('src', imgSource);
+				index = totalItems - 1;
+				Update();
 			}
 		}
 
+		function LoadContent(item) {
+			var tag = $(item)[0].localName;
 
+			if(tag == 'a') {
+				var videoID = $(item).attr('href').match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/)[2];
+				$('#alb_content').html(`<iframe src="https://www.youtube.com/embed/${videoID}?badge=0&html5=1" width="1280" height="720" frameborder="0" allowfullscreen></iframe>`);
+			}
+			else if(tag == 'img') {
+				$('#alb_content').html(`<img src="${$(item).attr('src')}"/>`);
+			}
+		}
 
-		/*
-				if(location.hash != "") {
-					Open(items[location.hash.substr(1)]);
-				}
-		*/
-
-		/*--Overlay öffnen--*/
-		$(this).find(itemSelector).stop().click(function() {
+		$(this).find(itemSelector).stop().click(function(e) {
+			e.preventDefault();
 			Open($(this));
 		});
 
-		/*--Overlay schließen--*/
-		$('#alb_icon_close').stop().click(function() {
-			Close();
-		});
+		$('#alb_icon_close').stop().click(function() { Close(); });
+		$('#alb_icon_next').stop().click(function() { Next(); });
+		$('#alb_icon_prev').stop().click(function() { Previous(); });
 
-		/*--Nächstes Bild--*/
-		$('#alb_icon_next').stop().click(function() {
-			NextItem(imgActive);
-		});
-
-		/*--Vorheriges Bild--*/
-		$('#alb_icon_prev').stop().click(function() {
-			PreviousItem(imgActive);
-		});
-
-		/*--Pfeiltasten-Steuerung--*/
 		$(document).keydown(function(e) {
-			/*--Pfeil nach Rechts--*/
-			if(e.keyCode == 39)
-				NextItem(imgActive);
-			/*--Pfeil nach Links--*/
-			if(e.keyCode == 37)
-				PreviousItem(imgActive);
-			/*--ESC-Taste--*/
-			if(e.keyCode == 27)
-				Close();
+			if(e.keyCode == 39) Next();
+			if(e.keyCode == 37) Previous();
+			if(e.keyCode == 27) Close();
 		});
 	};
 }(jQuery));
